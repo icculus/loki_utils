9e4a94619b49072c16603f6f94af33fea3b33381
megastep <megastep@efe55400-711e-0410-887b-a96ac87c7689>
2000-02-05 03:18:18 +0000

    Updated testargs for non-option arguments

--- trunk/testargs.c
+++ trunk/testargs.c
@@ -7,6 +7,7 @@ char *game_version = "Loki Utils Test Program";
 int main(int argc, char **argv)
 {
   const char *opt;
+  char **args;
 
   loki_registeroption("optarg", 'o', "\tOptional argument", LOKI_OPTSTRING);
   loki_registeroption("string", 'm', "\tRequired argument", LOKI_STRING);
@@ -18,5 +19,16 @@ int main(int argc, char **argv)
 	printf("optarg not specified\n");
   }
   printf("string = %s\n", loki_getconfig_str("string"));
+  args = loki_getarguments();
+  if(args) {
+	printf("Non-option arguments: ");
+	while( *args ) {
+	  printf("%s ", *args);
+	  args ++;
+	}
+	putchar('\n');
+  } else {
+	printf("Error, looks like the command line was not parsed!\n");
+  }
   return 0;
 }
