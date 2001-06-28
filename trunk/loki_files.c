edb00dc838424b47728d005f75bf0808e7c17bd5
hercules <hercules@efe55400-711e-0410-887b-a96ac87c7689>
2001-06-28 20:24:25 +0000

    Allow case insensitive file opening

--- trunk/loki_files.c
+++ trunk/loki_files.c
@@ -25,6 +25,7 @@
 #include <limits.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <dirent.h>
 
 #include "loki_utils.h"
 
@@ -124,39 +125,85 @@ FILE *loki_fopen(const char *file, const char *mode)
     return value;
 }
 
+/* A case insensitive open function */
+static int open_nocase(const char *path, const char *file,
+                       int flags, mode_t mode)
+{
+    char full_path[PATH_MAX];
+    int value;
+
+    snprintf(full_path, sizeof(full_path), "%s/%s", path, file);
+    value = open(full_path, flags, mode);
+    if ( value < 0 ) {
+        char *base, *sep;
+        DIR *dirp;
+        struct dirent *entry;
+
+        /* Get the base of the path */
+        base = full_path + strlen(path) + 1;
+        sep = strchr(base, '/');
+        if ( sep ) {
+            *sep = '\0';
+            file += (sep-base)+1;
+        } else {
+            file = 0;
+        }
+        sep = strrchr(full_path, '/');
+        *sep = '\0';
+
+        dirp = opendir(full_path);
+        if ( dirp ) {
+            *sep = '/';
+            while ( (value < 0) && ((entry=readdir(dirp)) != NULL) ) {
+                if ( strcasecmp(entry->d_name, base) == 0 ) {
+                    strcpy(base, entry->d_name);
+                    if ( file ) {
+                        value = open_nocase(full_path, file, flags, mode);
+                    } else {
+                        value = open(full_path, flags, mode);
+                    }
+                }
+            }
+            closedir(dirp);
+        }
+    }
+    return value;
+}
+
 int loki_open(const char *file, int flags, mode_t mode)
 {
-    char path[PATH_MAX];
-    int pass;
     int value;
 
     /* If it's a full pathname, we're fine */
     if ( *file == '/' ) {
-        mkdirhier(path);
+        mkdirhier(file);
         return open(file, flags, mode);
     }
 
     /* If we're writing, we must write to the preferences */
     if ( flags == O_RDONLY ) {
+        char *path = 0;
+        int pass;
+
         /* First look in preferences, then in data directory */
         value = -1;
         for ( pass = 0; (value < 0) && (pass < 3); ++pass ) {
             switch (pass) {
                 case 0:
-                    strcpy(path, loki_getprefpath());
+                    path = loki_getprefpath();
                     break;
                 case 1:
-                    strcpy(path, loki_getdatapath());
+                    path = loki_getdatapath();
                     break;
                 case 2:
-                    strcpy(path, ".");
+                    path = ".";
                     break;
             }
-            strcat(path, "/");
-            strcat(path, file);
-            value = open(path, O_RDONLY, 0);
+            value = open_nocase(path, file, O_RDONLY, 0);
         }
     } else {
+        char path[PATH_MAX];
+
         sprintf(path, "%s/%s", loki_getprefpath(), file);
         value = open(path, flags, mode);
         if ( (value < 0) && (flags & O_RDWR) ) {
